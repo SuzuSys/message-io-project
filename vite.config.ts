c8e80009f749067944a1309a18b666d10da2eee1
SuzuSys <sigmatics@outlook.jp>
2024-02-22 23:47:32 +0900

    some little change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'message-io-project',
+  base: '/message-io-project',
 })
